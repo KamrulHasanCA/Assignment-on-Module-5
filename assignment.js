710bc3602a7a0ee53215eddad92156edbbffa90d
Kamrul Hasan <kamrul.hasan.bift@gmail.com>
2026-08-16 23:53:03 -0500

    task one done

---
+++ assignment.js
@@ -0,0 +1,14 @@
+    // Task 1 : Variables & Constants
+
+    let myName = "Kamrul Hasan";
+    let myAge = 18;
+    let favoriteColor = "Black";
+
+    const country = "Bangladesh";
+
+    console.log("Name:", myName);
+    console.log("Age:", myAge);
+    console.log("Favorite Color:", favoriteColor);
+    console.log("Country:", country);
+
+
