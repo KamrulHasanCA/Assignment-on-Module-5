5330c7b1ab50bd9b024c1b94591a4690de178888
Kamrul Hasan <kamrul.hasan.bift@gmail.com>
2026-08-17 00:19:06 -0500

    task 2 and 3 done

--- assignment.js
+++ assignment.js
@@ -1,14 +1,51 @@
-    // Task 1 : Variables & Constants
+// Task 1 : Variables & Constants
 
-    let myName = "Kamrul Hasan";
-    let myAge = 18;
-    let favoriteColor = "Black";
+    // let myName = "Kamrul Hasan";
+    // let myAge = 18;
+    // let favoriteColor = "Black";
 
-    const country = "Bangladesh";
+    // const country = "Bangladesh";
 
-    console.log("Name:", myName);
-    console.log("Age:", myAge);
-    console.log("Favorite Color:", favoriteColor);
-    console.log("Country:", country);
+    // console.log("Name:", myName);
+    // console.log("Age:", myAge);
+    // console.log("Favorite Color:", favoriteColor);
+    // console.log("Country:", country);
 
+
+    // ============================================================================
+
+// Task 2: Data Types
+
+    // let studentName = "Kamrul";  //strings
+    // let rollNumber = 18;            //Number
+    // let isStudent = true;       //boolean
+    // let course = ["DNS", "Javasript", "OOP"];  //array
+    // let student = {name: "Kamrul", age: 18, student: true }; //object
+
+    // console.log(studentName, typeof studentName);
+    // console.log(rollNumber, typeof rollNumber);
+    // console.log(isStudent, typeof isStudent);
+    // console.log(course, typeof course);
+    // console.log(student, typeof student);
+
+
+
+    // ============================================================================
+
+
+    // Task 3 : Operators
+
+// let a = 20;
+// let b = 5;
+
+// console.log(a + b); //additon
+// console.log(a - b); //substract
+// console.log(a * b); //multiply
+// console.log(a / b); //division
+// console.log(a % b); //reminder
+
+
+// ============================================================================
+  
+    
 
